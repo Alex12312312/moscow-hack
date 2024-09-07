aa88687d3bf3de201c30ef3e4f429787be543ab5
Alex <sasha_novosel@mail.ru>
2024-09-07 18:27:14 +0300

    package-json

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-
-  experimental: {
-    appDir: true,
-  },
-
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      // Fix deps that don't work with vite
-      'stream': require.resolve('stream-browserify'),
-      'zlib': require.resolve('browserify-zlib'),
-      'util': require.resolve('util'),
-    }
-
-    // Important: return the modified config
-    return config
-  },
 };
 
 export default nextConfig;
