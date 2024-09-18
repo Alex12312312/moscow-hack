07f312ee22608249e6b54f1053020783c090a9df
cutemiya <k-shaipov@mail.ru>
2024-09-18 16:06:37 +0300

    linter

--- src/components/ui/Link/index.tsx
+++ src/components/ui/Link/index.tsx
@@ -1,16 +1,19 @@
-import { useRouter } from "next/navigation"
+import { useRouter } from 'next/navigation'
 
 interface ILink {
     path: string,
     text: string,
     disabled?: boolean,
 }
+
 export const Link = ({
-    path,
-    text,
-    disabled,
-}:ILink) =>{
+                         path,
+                         text,
+                         disabled,
+                     }: ILink) => {
     const nav = useRouter();
     return <button className="font-semibold text-accent-700 cursor-pointer
-    hover:text-accent-500 focus:text-accent-800 disabled:text-[#474747]" disabled={disabled} onClick={()=>{nav.push(path)}}>{text}</button>
+    hover:text-accent-500 focus:text-accent-800 disabled:text-[#474747]" disabled={disabled} onClick={() => {
+        nav.push(path)
+    }}>{text}</button>
 }
