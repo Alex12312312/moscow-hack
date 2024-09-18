211b4beefd96eb025067507fdf54ebcc1dd0be3d
Miya Furikadze <50241874+cutemiya@users.noreply.github.com>
2024-09-18 21:58:55 +0300

    home page init (#6)

--- src/lib/css.ts
+++ src/lib/css.ts
@@ -8,7 +8,7 @@ export type TailwindColor =
       | 600
       | 700
       | 800}`
-  | `${'error' | 'success' | 'info' | 'warning'}-${100 | 200 | 300 | 400} | ${'white' | 'black'}`;
+  | `${'error' | 'success' | 'info' | 'warning'}-${100 | 200 | 300 | 400}` | 'white' | 'black';
 
 export type TailwindColorText = `text-${TailwindColor}`;
 export type TailwindColorBg = `bg-${TailwindColor}`;
